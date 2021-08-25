ad05e041fa6c80120fcc3b538c04822db42102e3
brk-a <fnjakai@outlook.com>
2021-08-25 06:44:57 -0700

    parse

--- parsing_functions.c
+++ parsing_functions.c
@@ -1,4 +1,5 @@
 #include "olaf.h"
+
 /**
  * parse_line - Parses the command line looking for commands and argumements.
  * This function it is also in charged of freeing memory that is not longer
@@ -8,35 +9,36 @@
  * @command_counter: A counter keeping track of how many commands have been
  * entered into the shell.
  * @av: Name of the program running the shell
+ * Return: void
  */
 void parse_line(char *line, size_t size, int command_counter, char **av)
 {
-  int i;
-  ssize_t read_len;
-  int token_count;
-  char **param_array;
-  const char *delim = "\n\t ";
+int i;
+ssize_t read_len;
+int token_count;
+char **param_array;
+const char *delim = "\n\t ";
 
-  token_count = 0;
-  write(STDOUT_FILENO, PROMPT, str_len(PROMPT));
-  read_len = getline(&line, &size, stdin);
-  if (read_len != -1)
-    {
-      param_array = token_interface(line, delim, token_count);
-      if (param_array[0] == NULL)
-	{
-	  single_free(2, param_array, line);
-	  return;
-	}
-      i = built_in(param_array, line);
-      if (i == -1)
-	create_child(param_array, line, command_counter, av);
-      for (i = 0; param_array[i] != NULL; i++)
-	free(param_array[i]);
-      single_free(2, param_array, line);
-    }
-  else
-    exit_b(line);
+token_count = 0;
+write(STDOUT_FILENO, PROMPT, str_len(PROMPT));
+read_len = getline(&line, &size, stdin);
+if (read_len != -1)
+{
+param_array = token_interface(line, delim, token_count);
+if (param_array[0] == NULL)
+{
+single_free(2, param_array, line);
+return;
+}
+i = built_in(param_array, line);
+if (i == -1)
+create_child(param_array, line, command_counter, av);
+for (i = 0; param_array[i] != NULL; i++)
+free(param_array[i]);
+single_free(2, param_array, line);
+}
+else
+exit_b(line);
 }
 
 /**
@@ -47,48 +49,49 @@ void parse_line(char *line, size_t size, int command_counter, char **av)
  * @count: A counter keeping track of how many commands have been entered
  * into the shell.
  * @av: Name of the program running the shell
+ * Return: void
  */
 void create_child(char **param_array, char *line, int count, char **av)
 {
-  pid_t id;
-  int status;
-  int i;
-  int check;
-  struct stat buf;
-  char *tmp_command;
-  char *command;
+pid_t id;
+int status;
+int i;
+int check;
+struct stat buf;
+char *tmp_command;
+char *command;
 
-  id = fork();
-  if (id == 0)
-    {
-      tmp_command = param_array[0];
-      command = path_finder(param_array[0]);
-      if (command == NULL)
-	{
-	  /*Looking for file in current directory*/
-	  check = stat(tmp_command, &buf);
-	  if (check == -1)
-	    {
-	      error_printing(av[0], count, tmp_command);
-	      print_str(": not found", 0);
-	      single_free(2, line, tmp_command);
-	      for (i = 1; param_array[i]; i++)
-		free(param_array[i]);
-	      free(param_array);
-	      exit(100);
-	    }
-	  /*file exist in cwd or has full path*/
-	  command = tmp_command;
-	}
-      param_array[0] = command;
-      if (param_array[0] != NULL)
-	{
-	  if (execve(param_array[0], param_array, environ) == -1)
-	    exec_error(av[0], count, tmp_command);
-	}
-    }
-  else
-    wait(&status);
+id = fork();
+if (id == 0)
+{
+tmp_command = param_array[0];
+command = path_finder(param_array[0]);
+if (command == NULL)
+{
+/*Looking for file in current directory*/
+check = stat(tmp_command, &buf);
+if (check == -1)
+{
+error_printing(av[0], count, tmp_command);
+print_str(": not found", 0);
+single_free(2, line, tmp_command);
+for (i = 1; param_array[i]; i++)
+free(param_array[i]);
+free(param_array);
+exit(100);
+}
+/*file exist in cwd or has full path*/
+command = tmp_command;
+}
+param_array[0] = command;
+if (param_array[0] != NULL)
+{
+if (execve(param_array[0], param_array, environ) == -1)
+exec_error(av[0], count, tmp_command);
+}
+}
+else
+wait(&status);
 }
 
 /**
@@ -100,24 +103,25 @@ void create_child(char **param_array, char *line, int count, char **av)
  * Return: Upon success an array of tokens representing the command. Otherwise
  * returns NULL.
  */
+
 char **token_interface(char *line, const char *delim, int token_count)
 {
-  char **param_array;
+char **param_array;
 
-  token_count = count_token(line, delim);
-  if (token_count == -1)
-    {
-      free(line);
-      return (NULL);
-    }
-  param_array = tokenize(token_count, line, delim);
-  if (param_array == NULL)
-    {
-      free(line);
-      return (NULL);
-    }
+token_count = count_token(line, delim);
+if (token_count == -1)
+{
+free(line);
+return (NULL);
+}
+param_array = tokenize(token_count, line, delim);
+if (param_array == NULL)
+{
+free(line);
+return (NULL);
+}
 
-  return (param_array);
+return (param_array);
 }
 
 /**
@@ -131,24 +135,24 @@ char **token_interface(char *line, const char *delim, int token_count)
  */
 char **tokenize(int token_count, char *line, const char *delim)
 {
-  int i;
-  char **buffer;
-  char *token;
-  char *line_cp;
+int i;
+char **buffer;
+char *token;
+char *line_cp;
 
-  line_cp = _strdup(line);
-  buffer = malloc(sizeof(char *) * (token_count + 1));
-  if (buffer == NULL)
-    return (NULL);
-  token = strtok(line_cp, delim);
-  for (i = 0; token != NULL; i++)
-    {
-      buffer[i] = _strdup(token);
-      token = strtok(NULL, delim);
-    }
-  buffer[i] = NULL;
-  free(line_cp);
-  return (buffer);
+line_cp = _strdup(line);
+buffer = malloc(sizeof(char *) * (token_count + 1));
+if (buffer == NULL)
+return (NULL);
+token = strtok(line_cp, delim);
+for (i = 0; token != NULL; i++)
+{
+buffer[i] = _strdup(token);
+token = strtok(NULL, delim);
+}
+buffer[i] = NULL;
+free(line_cp);
+return (buffer);
 }
 
 /**
@@ -159,16 +163,16 @@ char **tokenize(int token_count, char *line, const char *delim)
  */
 int count_token(char *line, const char *delim)
 {
-  char *str;
-  char *token;
-  int i;
+char *str;
+char *token;
+int i;
 
-  str = _strdup(line);
-  if (str == NULL)
-    return (-1);
-  token = strtok(str, delim);
-  for (i = 0; token != NULL; i++)
-    token = strtok(NULL, delim);
-  free(str);
-  return (i);
+str = _strdup(line);
+if (str == NULL)
+return (-1);
+token = strtok(str, delim);
+for (i = 0; token != NULL; i++)
+token = strtok(NULL, delim);
+free(str);
+return (i);
 }

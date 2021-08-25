d42dc29317eed9802b7ba17839ec126957ed4db9
brk-a <fnjakai@outlook.com>
2021-08-25 05:03:25 -0700

    0x16: simple shell try v 2.1; changes to algo

--- shell.c
+++ shell.c
@@ -1,59 +1,9 @@
-#include "shell.h"
-
-int exitcode = 0;
-int errorcount = 0;
-
-/**
- * main - a simple shell program written in C
- * @argc: number of arguments
- * @argv: array of arguments
- * @env: array of environment variables
- *
- * Return: 0 always (but program may exit early)
- */
-
-int main(__attribute__((unused)) int argc, char **argv, char **env)
-{
-  char *user_input = NULL;
-  char **commands = NULL;
-  char **path_array = NULL;
-  size_t nbytes = 0;
-  ssize_t bytes_read = 0;
-  char *NAME = argv[0];
-  int atty_is = isatty(0);
-
-  signal(SIGINT, SIG_IGN);
-
- 
-
-  while (1)
-    {
-      errorcount++;
-      if (atty_is)
-	write(STDOUT_FILENO, “simple_shell$ \", 13);
-bytes_read = getline(&user_input, &nbytes, stdin);
-if (bytes_read == -1)
-{
-free(user_input);
-exit(exitcode);
-}
-if (exit_check(user_input, NAME) == -1)
-continue;
-if (blank_check(user_input) == 1)
-continue;
-if (env_check(user_input) == 1)
-{
-print_env(env);
-continue;
-}
-path_array = get_path_array(env);
-commands = parse_input(user_input, path_array, NAME);
-if (commands != NULL)
-{
-fork_wait_exec(commands, path_array, env, NAME, user_input);
-free_array(commands);
-free_array(path_array);
-}
-}
-return (0);
+#include "olaf.h"
+/** * main - Entry point of the program. * @ac: The number of parameters passed to the executable file. In the case * 
+ this variable will not be used. * @av: The name of the program. * Return: Always 0. */
+int main(__attribute__((unused)) int ac, char **av) { char *line; size_t size; int command_counter; command_counter = 
+	0; signal(SIGINT, SIG_IGN); do {
+		command_counter++; line = NULL; size = 0; parse_line(line, size, command_counter, av);
+	} while (1);
+	return (0);
 }
